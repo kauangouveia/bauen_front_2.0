c8fd6df472d2e13df2dd317cce9792581904dce6
kauangouveia <kauangouveia64@gmail.com>
2022-02-10 17:45:43 -0300

    iniciado projeto em next

---
+++ src/global/index.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyles = createGlobalStyle`
+* {
+    font-family: "Baloo 2";
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    overflow-x: hidden;
+}
+`;
+
+export default GlobalStyles;
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,14 @@
+import Container from "../../components/Container";
+import GlobalStyles from "../../global";
+
+
+function Login() {
+  return (
+    <>
+      <GlobalStyles />
+      <Container />
+    </>
+  );
+}
+
+export default Login;
